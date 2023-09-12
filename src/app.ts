15e40d59aaefa509dc6fc6a9aad0f2a74b5bd7b6
topus009 <sj_89@mail.ru>
2023-09-12 16:53:30 +0300

    fix back-url

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import TelegramBot from "node-telegram-bot-api";
 import axios from 'axios';
 
 const bot = new TelegramBot(`${process.env.TOKEN}`, {polling: true});
+const BACKEND_URL = process.env.BACKEND_URL;
 
 (async () => {
   bot.on("message", async (msg) => {
     if (msg?.from?.id) {
       const res = await axios({
-        url: `${process.env.BACKEND_URL}`,
+        url: BACKEND_URL,
         method: 'GET',
         responseType: 'arraybuffer'
       });
