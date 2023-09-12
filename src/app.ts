bce63f93ba043ae89a32d9d7a5ef2afeeffda46b
topus009 <sj_89@mail.ru>
2023-09-12 21:44:01 +0300

    fix fastify

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,82 @@
-import TelegramBot, {ConstructorOptions} from "node-telegram-bot-api";
-import axios from 'axios';
+import process from 'node:process';
 
-const options: ConstructorOptions = {
-  polling: true,
+import fastify, {
+  // fastify,
+  FastifyInstance,
+  FastifyListenOptions,
+} from "fastify";
+
+import {Telegraf} from "telegraf";
+import healthCheck from 'fastify-healthcheck';
+
+const {
+  NODE_ENV,
+  BACKEND_URL,
+  FASTTIFY_PORT,
+  BOT_PORT,
+  BOT_TOKEN: token,
+  WEBHOOK_DOMAIN: webhookDomain,
+} = process.env;
+
+console.log({
+  NODE_ENV,
+  BACKEND_URL,
+  FASTTIFY_PORT,
+  BOT_PORT,
+  token,
+  webhookDomain,
+})
+
+const isProd = NODE_ENV === "production"
+
+const port = parseInt(FASTTIFY_PORT || "3001", 10);
+const bot_port = parseInt(BOT_PORT || "3000", 10);
+const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
+const serverOpts: FastifyListenOptions = {
+  port,
+  host,
 }
+const bot = new Telegraf(`${token}`);
+const app = fastify();
 
-const bot = new TelegramBot(`${process.env.TOKEN}`, options);
-const BACKEND_URL = process.env.BACKEND_URL;
+if (isProd) {
+  if (!webhookDomain) throw new Error('"WEBHOOK_DOMAIN" env var is required!');
 
-const init = async () => {
-  bot.on("message", async (msg) => {
-    if (msg?.from?.id) {
-      try {
-        const res = await axios({
-          url: BACKEND_URL,
-          method: 'GET',
-          responseType: 'arraybuffer'
-        });
+  app.post(bot.secretPathComponent(), async (req, rep) => {
+    const webhook = await bot.createWebhook({
+      domain: webhookDomain
+    });
 
-        await bot.sendPhoto(msg.from.id, res.data);
-      } catch (error) {
-        console.log(error)
-      }
-    }
+    return webhook(req.raw, rep.raw)
   });
 }
 
-init();
+app.register(healthCheck)
+
+bot.start((ctx) => ctx.reply('Welcome'))
+
+bot.on("message", ctx => {
+  if (BACKEND_URL) {
+    return ctx.replyWithPhoto({
+      url: BACKEND_URL
+    })
+  } else {
+    return ctx.reply(`BACKEND_URL is invalid`)
+  }
+});
+
+bot
+  .launch()
+  .then(() => console.log("Webhook bot listening on port", BOT_PORT));
+
+app.listen(serverOpts, (err, address) => {
+  console.log("Listening on port", port)
+
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+});
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
